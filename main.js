c1e55e1391072f7bcd90021ee5750e87a8e650b6
Santiago Scarpa <santiago.scarpa@gmail.com>
2025-02-13 00:18:18 -0300

    agrego color de fondo y re escalado de imagen, con el barco ya cargad

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-//import settings from './settings.json' with {type: 'json'};
-import { gameScene } from './modules/game'
+import { gameScene } from './modules/game.js'
 
 const config = {//Objeto global que viene en archivo min de Phaser 
     type: Phaser.AUTO, // tipo de renderizado para el juego
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     backgroundColor: '#8AC4FF',
     parent: 'game', //contenedor donde se va a renderizar el juego, es el div que esta en el html
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 },
+            gravity: { y: 0 },
             debug: false
         }
+
+    },
+    scale: {
+        // Fit to window
+        mode: Phaser.Scale.FIT,
+        // Center vertically and horizontally
+        autoCenter: Phaser.Scale.CENTER_BOTH
     },
     scene:
         [gameScene]
--- modules/game.js
+++ modules/game.js
@@ -1,13 +1,20 @@
-import settings from './settings.json' with {type: 'json'};
+import settings from './../settings.json' with {type: 'json'};
+
 export class gameScene extends Phaser.Scene {
     constructor() {
         super("gameScene")
     }
     preload() {
-        console.log(`version ${settings.version}`)
+        console.log(`version ${settings.version}`);
+
+        this.load.spritesheet('bismarck',
+            './assets/imgs/sprites/bismarckTransparente.PNG',
+            { frameWidth: 828, frameHeight: 145 }
+        );
     }
     create() {
-
+        this.bismarck = this.physics.add.sprite(50, 250, 'bismarck')
+        this.bismarck.setScale(0.10)
     }
     update() {
 
